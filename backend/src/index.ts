25e3d34b5932edbb94d1f3f02380a7c60f0625fc
Herwiggg <michelot.alexy78@gmail.com>
2025-07-04 18:59:50 +0200

    docker done

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+import fastifySqlite from "fastify-sqlite";
+import jwt from "@fastify/jwt";
+
+const app = fastify();
+app.register(jwt, { secret: process.env.JWT_SECRET || "devsecret" });
+app.register(fastifySqlite, {
+  promiseApi: true,
+  dbPath: process.env.DB_PATH || "/data/main.db"
+});
+
+app.get("/api/ping", async () => ({ pong: true }));
+app.listen({ host: "0.0.0.0", port: 3000 });
