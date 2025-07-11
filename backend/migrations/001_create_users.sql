ceabb1c7aa46bd89cbe49c23aeb8dcd94dc8da1d
Herwiggg <michelot.alexy78@gmail.com>
2025-07-11 18:36:01 +0200

    added default avatar db

--- backend/migrations/001_create_users.sql
+++ backend/migrations/001_create_users.sql
@@ -3,7 +3,9 @@ CREATE TABLE IF NOT EXISTS users (
   pseudo      TEXT    NOT NULL UNIQUE,
   email       TEXT    NOT NULL UNIQUE,
   pwd_hash    TEXT    NOT NULL,
+  nb_games    INTEGER NOT NULL DEFAULT 0,
   is2fa       INTEGER NOT NULL DEFAULT 0,
+  avatar      TEXT    NOT NULL DEFAULT '/static/avatars/default.png',
   locale      TEXT,
   created_at  DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%f','now')),
   updated_at  DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%f','now'))
